b5e50ee57004ea19f8d8a06da41a527fb327baf9
shashankmarch27 <shashankmarch27@gmail.com>
2024-03-23 21:37:20 +0530

    Dshot150 and Dshot300 Protocol added

--- src/DriveMaster.cpp
+++ src/DriveMaster.cpp
@@ -1,9 +1,12 @@
 #include "DriveMaster.h"
 
-DriveMaster::DriveMaster(int pin): _pin(pin){}
+DriveMaster::DriveMaster(int pin) : _pin(pin) {}
 
-DriveMaster::~DriveMaster(){}
+DriveMaster::~DriveMaster() {}
 
-void DriveMaster::begin(){}
+void DriveMaster::begin() {}
 
-void DriveMaster::write(){}
+void DriveMaster::write(uint16_t value, bool telemetery) {}
+
+void DriveMaster::sendCommand(uint16_t value) {}
+void DriveMaster::sendValue(uint16_t value) {}
--- src/dshot.h
+++ src/dshot.h
@@ -3,17 +3,28 @@
 #define DSHOT_H
 
 #include "DriveMaster.h"
+#include <esp32-hal-rmt.h>
+#define DSHOT_FRAME_LENGTH 16
+
+enum DShotType { DSHOT_150, DSHOT_300 };
 
 class dshot : public DriveMaster {
 private:
   rmt_obj_t *_channel = nullptr;
-  rmt_data_t _data[16];
+  rmt_data_t _data[DSHOT_FRAME_LENGTH];
+  uint16_t _timingDuration0;
+  uint16_t _timingDuration1;
+  uint16_t _timingDurationBit;
+  uint8_t calculateCrc(uint16_t value);
+  void write(uint16_t value, bool telemetery = false);
+
 public:
-  explicit dshot(int pin);
+  explicit dshot(int pin, DShotType type = DSHOT_300);
   ~dshot();
 
   void begin() override;
-  void write() override;
+  void sendCommand(uint16_t value) override;
+  void sendValue(uint16_t value) override;
 };
 
 #endif
